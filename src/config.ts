f80b9502e536e7b1140e33caea6ad456e5935dfa
FinPluto <35735190+finpluto@users.noreply.github.com>
2025-04-03 12:34:09 +0200

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,22 @@
+export const SITE = {
+  website: "https://devblog.finpluto.tech", // replace this with your deployed domain
+  author: "Hengyi Lin",
+  profile: "https://github.com/finpluto",
+  desc: "Dev Blog for KTH Course DH2650",
+  title: "DH2650-devblog",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerIndex: 4,
+  postPerPage: 4,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  showArchives: true,
+  showBackButton: true, // show back button in post detail
+  editPost: {
+    enabled: false,
+    text: "Suggest Changes",
+    url: "https://github.com/satnaing/astro-paper/edit/main/",
+  },
+  dynamicOgImage: true,
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+} as const;
